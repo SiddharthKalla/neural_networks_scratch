2420622178d684c0e7b9f046d34a2936bd998733
ADITYA964 <61913852+ADITYA964@users.noreply.github.com>
2020-07-03 19:48:41 -1200

    Add files via upload

---
+++ Aditya_Bagwadkar_files/NeuralNetwork.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+class NeuralNetwork:
+    
+    def __init__(self,input_size,layers,output_size):
+        np.random.seed(0)
+        
+        model = {} #Dictionary
+        
+        #First Layer
+        model['W1'] = np.random.randn(input_size,layers[0])
+        model['b1'] = np.zeros((1,layers[0]))
+        
+        #Second Layer
+        model['W2'] = np.random.randn(layers[0],layers[1])
+        model['b2'] = np.zeros((1,layers[1]))
+        
+        #Third/Output Layer
+        model['W3'] = np.random.randn(layers[1],output_size)
+        model['b3'] = np.zeros((1,output_size))
+        
+        self.model = model
+        self.activation_outputs = None
+        self.x = None
+    
+    def forward(self,x):
+        
+        W1,W2,W3 = self.model['W1'],self.model['W2'],self.model['W3']
+        b1, b2, b3 = self.model['b1'],self.model['b2'],self.model['b3']
+        
+        z1 = np.dot(x,W1) + b1
+        a1 = np.tanh(z1) 
+        print("Input shape = {} Output shape = {}".format(x.shape,a1.shape))
+        
+        z2 = np.dot(a1,W2) + b2
+        a2 = np.tanh(z2)
+        print("Input shape = {} Output shape = {}".format(a1.shape,a2.shape))
+        
+        z3 = np.dot(a2,W3) + b3
+        y_ = softmax(z3)
+        print("Input shape = {} Output shape = {}".format(a2.shape,y_.shape))
+        
+        self.x = x
+        self.activation_outputs = (a1,a2,y_)
+        return y_
+        
+    def backward(self,y,learning_rate=0.001):
+        W1,W2,W3 = self.model['W1'],self.model['W2'],self.model['W3']
+        b1, b2, b3 = self.model['b1'],self.model['b2'],self.model['b3']
+        x = self.x
+        m = x.shape[0]
+        
+        a1,a2,y_ = self.activation_outputs
+        
+        delta3 = y_ - y
+        dw3 = np.dot(a2.T,delta3)
+        db3 = np.sum(delta3,axis=0)
+        
+        delta2 = (1-np.square(a2))*np.dot(delta3,W3.T)
+        dw2 = np.dot(a1.T,delta2)
+        db2 = np.sum(delta2,axis=0)
+        
+        delta1 = (1-np.square(a1))*np.dot(delta2,W2.T)
+        dw1 = np.dot(x.T,delta1)
+        db1 = np.sum(delta1,axis=0)
+        
+        
+        #Update the Model Parameters using Gradient Descent
+        self.model["W1"]  -= learning_rate*dw1
+        self.model['b1']  -= learning_rate*db1
+        
+        self.model["W2"]  -= learning_rate*dw2
+        self.model['b2']  -= learning_rate*db2
+        
+        self.model["W3"]  -= learning_rate*dw3
+        self.model['b3']  -= learning_rate*db3
+        
+        return np.dot(delta1,W1.T)
+        
+    def predict(self,x):
+        y_out = self.forward(x)
+        return np.argmax(y_out,axis=1)
+
+
+# In[3]:
+
+
+def loss(y_oht,p):
+    l = -np.mean(y_oht*np.log(p))
+    return l
+
+def one_hot(y,depth):
+    
+    m = y.shape[0]
+    y_oht = np.zeros((m,depth))
+    y_oht[np.arange(m),y] = 1
+    return y_oht
+
+
+# In[4]:
+
+
+def softmax(a):
+    e_pa = np.exp(a) #Vector
+    ans = e_pa/np.sum(e_pa,axis=1,keepdims=True)
+    return ans
