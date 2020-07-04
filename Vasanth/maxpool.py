da18f54576ec7f3467083972a901064cdfffa3b5
Vasanth Premala <53056749+VasanthPremala@users.noreply.github.com>
2020-07-04 12:47:58 +0530

    Add files via upload

---
+++ Vasanth/maxpool.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+class MaxPool2:
+  #Forward
+
+  def forward(self, input):
+    '''
+    Performs a forward pass of the maxpool layer using the given input.
+    Returns a 3d numpy array with dimensions (h / 2, w / 2, num_filters).
+    - input is a 3d numpy array with dimensions (h, w, num_filters)
+    '''
+    self.last_input = input
+  
+
+
+# In[15]:
+
+
+class MaxPool2:
+  # Backward
+
+  def iterate_regions(self, image):
+    '''
+    Generates non-overlapping 2x2 image regions to pool over.
+    - image is a 2d numpy array
+    '''
+    h, w, _ = image.shape
+    new_h = h // 2
+    new_w = w // 2
+
+    for i in range(new_h):
+      for j in range(new_w):
+        im_region = image[(i * 2):(i * 2 + 2), (j * 2):(j * 2 + 2)]
+        yield im_region, i, j
+
+  def backprop(self, d_L_d_out):
+    '''
+    Performs a backward pass of the maxpool layer.
+    Returns the loss gradient for this layer's inputs.
+    - d_L_d_out is the loss gradient for this layer's outputs.
+    '''
+    d_L_d_input = np.zeros(self.last_input.shape)
+
+    for im_region, i, j in self.iterate_regions(self.last_input):
+      h, w, f = im_region.shape
+      amax = np.amax(im_region, axis=(0, 1))
+
+      for i2 in range(h):
+        for j2 in range(w):
+          for f2 in range(f):
+            # If this pixel was the max value, copy the gradient to it.
+            if im_region[i2, j2, f2] == amax[f2]:
+              d_L_d_input[i * 2 + i2, j * 2 + j2, f2] = d_L_d_out[i, j, f2]
+
+    return d_L_d_input
+
+
+# In[ ]:
+
+
+
+
